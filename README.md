4029cfb2e1d0c5df7054300448f851ef962c32bb
tranminhtoan149 <tranminhtoan.149@gmail.com>
2023-04-03 21:25:35 +0700

    add source

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # DoAnChuyenNganh
+
+How to run:
+
+1. Install:
+   pip install gpt_index
+2. If add more data:
+   add data file .txt to folder data
+3. To train data:
+   python train.py
+4. To run:
+   python main.py
---
+++ main.py
@@ -0,0 +1,15 @@
+from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain import OpenAI
+import sys
+import os
+os.environ["OPENAI_API_KEY"] = "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"
+
+def answer_me(vector_index):
+    v_index = GPTSimpleVectorIndex.load_from_disk(vector_index)
+    while True:
+        user_input = input('Please ask: ')
+        response = v_index.query(user_input, response_mode='compact')
+        print(f'Response: {response} \n')
+
+
+answer_me('vector_index.json')
